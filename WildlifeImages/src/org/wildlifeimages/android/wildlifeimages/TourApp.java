18b4c4bd4a2a34a9b33919816fc6bb35f40f7f9f
gmangw <wilkinsg@onid.orst.edu>
2011-12-15 17:11:12 -0800

    Added code scan.

--- WildlifeImages/src/org/wildlifeimages/android/wildlifeimages/TourApp.java
+++ WildlifeImages/src/org/wildlifeimages/android/wildlifeimages/TourApp.java
@@ -1,6 +1,12 @@
 package org.wildlifeimages.android.wildlifeimages;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
@@ -77,12 +83,12 @@ public class TourApp extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-
+        
         menu.add(0, MENU_HOME, 0, R.string.menu_home);
         menu.add(0, MENU_MAP, 0, R.string.menu_map);
         menu.add(0, MENU_SCAN, 0, R.string.menu_scan);
         menu.add(0, MENU_CAMERA, 0, R.string.menu_camera);
-
+        
         return true;
     }
 
@@ -103,9 +109,16 @@ public class TourApp extends Activity {
             	mapInit();
                 return true;
             case MENU_SCAN:
-            	Toast.makeText(this.getApplicationContext(), "Scan", 1).show(); //TODO
-            	mCamera.stopPreview();
-            	mCamera.release();
+            	boolean scanAvailable = isIntentAvailable(this, "com.google.zxing.client.android.SCAN");
+            	
+            	if (scanAvailable){
+            		mapInit();
+            		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            		intent.putExtra("com.google.zxing.client.android.SCAN.SCAN_MODE", "QR_CODE_MODE");
+                    startActivityForResult(intent, 0);
+            	} else {
+            		Toast.makeText(this.getApplicationContext(), "Install the Barcode Scanner app first.", 1).show();
+            	}
                 return true;
             case MENU_CAMERA:
             	//Toast.makeText(this.getApplicationContext(), "Camera", 1).show(); //TODO
@@ -131,7 +144,7 @@ public class TourApp extends Activity {
 
         // turn off the window's title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-
+        
         introInit();
         
         if (savedInstanceState == null) {
@@ -165,5 +178,45 @@ public class TourApp extends Activity {
     
     public void takePicture(View view) {
     	((CameraDisplay)findViewById(R.id.cam)).takePicture();
+    }
+    
+    /**
+     * Indicates whether the specified action can be used as an intent. This
+     * method queries the package manager for installed packages that can
+     * respond to an intent with the specified action. If no suitable package is
+     * found, this method returns false.
+     * 
+     * http://developer.android.com/resources/articles/can-i-use-this-intent.html
+     *
+     * @param context The application's environment.
+     * @param action The Intent action to check for availability.
+     *
+     * @return True if an Intent with the specified action can be sent and
+     *         responded to, false otherwise.
+     */
+    public static boolean isIntentAvailable(Context context, String action) {
+        final PackageManager packageManager = context.getPackageManager();
+        final Intent intent = new Intent(action);
+        List<ResolveInfo> list =
+                packageManager.queryIntentActivities(intent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+    
+    /**
+     * http://stackoverflow.com/questions/2050263/using-zxing-to-create-an-android-barcode-scanning-app
+     */
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                if (format.equals("QR_CODE")){
+                	Toast.makeText(this.getApplicationContext(), contents, 1).show();
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                //TODO
+            }
+        }
     }
 }
