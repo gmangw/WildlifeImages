70f839bf2115bb9cc03f0fdf900f97ed382add28
gmangw <wilkinsg@onid.orst.edu>
2012-02-13 15:57:07 -0800

    Expanding graphical update tool.

--- WildlifeImages Update Tool/src/org/wildlifeimages/tools/update/ZipManager.java
+++ WildlifeImages Update Tool/src/org/wildlifeimages/tools/update/ZipManager.java
@@ -17,43 +17,79 @@ import java.util.zip.ZipOutputStream;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListModel;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
 import javax.swing.filechooser.FileFilter;
 
 public class ZipManager extends JFrame implements ActionListener{
+
+	private static final long serialVersionUID = 1L;
+
 	private static final String assetPath = "assets/";
 
 	private final HashMap<String, byte[]> modifiedFiles = new HashMap<String, byte[]>();
 
-	Button saveButton = new Button("Save");
-	Button addButton = new Button("Open");
+	private String[] originalFiles;
+
+	private final Button saveButton = new Button("Save");
+	private final Button addButton = new Button("Open");
+	private final JList existingList = new JList();
+	private final JList modifiedList = new JList();
+
+	private final JPanel listPanel = new JPanel(new GridLayout(1,2, 5, 5));
+	private final JScrollPane listScrollPane = new JScrollPane(listPanel);
+
+	private final ExistingListModel modifiedListModel;
 
 	public static void main(String[] args){
 		ZipManager zr = new ZipManager();
-
-		String[] files = zr.readAPK("WildlifeImages.apk");
-
-		for (String s : files){
-			System.out.println(s);
-		}
+		zr.setVisible(true);
 	}
 
 	public ZipManager(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		saveButton.addActionListener(this);
 		saveButton.setSize(100, 20);
 
 		addButton.addActionListener(this);
 		addButton.setSize(100, 20);
+		
+		originalFiles = this.readAPK("WildlifeImages.apk");
+
+		existingList.setModel(new ExistingListModel());
+
+		modifiedListModel = new ExistingListModel(){
+			@Override
+			public Object getElementAt(int index){
+				if (modifiedFiles.containsKey(originalFiles[index])){
+					byte[] content = modifiedFiles.get(originalFiles[index]);
+					return new String(content, 0, Math.min(10, content.length));
+				}else{
+					return " ";
+				}
+			}
+		};
+
+		modifiedList.setModel(modifiedListModel);
+
+		listPanel.add(existingList);
+		listPanel.add(modifiedList);
 
 		this.setSize(640, 480);
-		this.setLayout(new GridLayout(1,2));
+		this.setLayout(new GridLayout(3,1));
 		this.add(addButton);
 		this.add(saveButton);
-
-		this.setVisible(true);
+		this.add(listScrollPane);
 	}
 
 	public void addFile(String filename, byte[] data){
 		modifiedFiles.put(filename, data);
+		modifiedListModel.notifyChange();
 	}
 
 	public String[] readAPK(String outFilename){
@@ -107,7 +143,6 @@ public class ZipManager extends JFrame implements ActionListener{
 		public String getDescription() {
 			return "Zip files (*.zip)";
 		}
-
 	}
 
 	@Override
@@ -124,6 +159,36 @@ public class ZipManager extends JFrame implements ActionListener{
 			}
 		}else if (event.getSource().equals(addButton)){
 			this.addFile("ExhibitContents/deltaIntro.html", "Updated delta intro page content.".getBytes());
+		}
+	}
+
+	private class ExistingListModel implements ListModel{
+		ArrayList<ListDataListener> listeners = new ArrayList<ListDataListener>();
+
+		@Override
+		public void addListDataListener(ListDataListener newListener) {
+			listeners.add(newListener);
+		}
+
+		@Override
+		public Object getElementAt(int index) {
+			return originalFiles[index];
+		}
+
+		@Override
+		public int getSize() {
+			return originalFiles.length;
+		}
+
+		@Override
+		public void removeListDataListener(ListDataListener oldListener) {
+			listeners.remove(oldListener);
+		}
+
+		public void notifyChange(){
+			for (ListDataListener listener : listeners){
+				listener.contentsChanged(new ListDataEvent(this, ListDataEvent.CONTENTS_CHANGED, 0, originalFiles.length));
+			}
 		}
 	}
 }
