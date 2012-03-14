075dd0ba313cae960ceea2790a9fd688ce1954c2
gmangw <wilkinsg@onid.orst.edu>
2012-03-13 18:27:49 -0700

    Prevented photo activity from crashing due to photo leaks.

--- WildlifeImages/src/org/wildlifeimages/android/wildlifeimages/PhotosActivity.java
+++ WildlifeImages/src/org/wildlifeimages/android/wildlifeimages/PhotosActivity.java
@@ -17,6 +17,23 @@ public class PhotosActivity extends WireActivity {
 		}
 	}
 
+	@Override
+	protected void onResume(){
+		super.onResume();
+	
+		ExhibitView exView = (ExhibitView) findViewById(R.id.photos_view);
+		String[] introPhotoList = getResources().getStringArray(R.array.intro_image_list);
+		exView.loadUrlList(introPhotoList, ContentManager.getSelf());
+	}
+	
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		
+		ExhibitView exView = (ExhibitView) findViewById(R.id.photos_view);
+		exView.clear();
+	}
+	
 	public static void start(Activity context, boolean showEvents) {
 		Intent photosIntent = new Intent(context, PhotosActivity.class);
 		photosIntent.putExtra("showEvents", showEvents);
